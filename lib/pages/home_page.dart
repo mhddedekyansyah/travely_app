d61044eef6065b24c6164960d00e3a0876811fae
Mhd Dedek Yansyah <dedek2203@gmail.com>
2022-04-09 12:08:44 +0700

    second commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,59 +1,64 @@
 part of 'pages.dart';
 
 class HomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
+      body: Stack(children: [
         SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Navbar(),
-              HeroContent(),
-              Category(),
-              ForYou()
-            ],
+          child: ListView(
+            children: [Navbar(), HeroContent(), Category(), const ForYou()],
           ),
         ),
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 15),
-            height: 90,
-            width: double.infinity,
-            color: Colors.white,
-            child:
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Column(
-                      children: [
-                        Image.asset('assets/icons/home.png', width: 26, height: 26,),
-                        SizedBox(height: 5,),
-                        Expanded(
-                          child: Container(
-                            height: 7,
-                            width: 7,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.black
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                    Image.asset('assets/icons/discovery.png', width: 26, height: 26,),
-                    Image.asset('assets/icons/bookmark_nav.png', width: 26, height: 26,),
-                    Image.asset('assets/icons/person.png', width: 26, height: 26,),
-                  ],
-                )
-          ),
+              padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 15),
+              height: 90,
+              width: double.infinity,
+              color: Colors.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      Image.asset(
+                        'assets/icons/home.png',
+                        width: 26,
+                        height: 26,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 7,
+                          width: 7,
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.circle, color: Colors.black),
+                        ),
+                      )
+                    ],
+                  ),
+                  Image.asset(
+                    'assets/icons/discovery.png',
+                    width: 26,
+                    height: 26,
+                  ),
+                  Image.asset(
+                    'assets/icons/bookmark_nav.png',
+                    width: 26,
+                    height: 26,
+                  ),
+                  Image.asset(
+                    'assets/icons/person.png',
+                    width: 26,
+                    height: 26,
+                  ),
+                ],
+              )),
         )
-        ]
-      ),
+      ]),
     );
   }
 }
--- lib/pages/widgets/hero_content.dart
+++ lib/pages/widgets/hero_content.dart
@@ -1,7 +1,8 @@
 part of 'widgets.dart';
+
 class HeroContent extends StatelessWidget {
-final List<Place> place = Place.mockPlace();
-  @override 
+  final List<Place> place = Place.mockPlace();
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(mainMargin),
@@ -9,80 +10,85 @@ final List<Place> place = Place.mockPlace();
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text("Let's go trip with us!", style: GoogleFonts.mulish(
-            fontSize: 24,
-            fontWeight: FontWeight.w600
+          Text(
+            "Let's go trip with us!",
+            style:
+                GoogleFonts.mulish(fontSize: 24, fontWeight: FontWeight.w600),
           ),
-        ),
-        const SizedBox(height: 15,),
-        Container(
-          padding: const EdgeInsets.all(25),
-          width: double.infinity,
-          height: 210,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),
-            image: DecorationImage(
-              image: AssetImage(place.first.url.toString()),
-              fit: BoxFit.cover
-            )
+          const SizedBox(
+            height: 15,
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(2.5),
-                    width: 45,
-                    height: 25,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.withOpacity(.8),
-                      borderRadius: BorderRadius.circular(20)
+          Container(
+            padding: const EdgeInsets.all(25),
+            width: double.infinity,
+            height: 210,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                image: DecorationImage(
+                    image: AssetImage(place.first.url.toString()),
+                    fit: BoxFit.cover)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(2.5),
+                      width: 45,
+                      height: 25,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(.8),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            'assets/icons/award.png',
+                            width: 21,
+                            height: 21,
+                          ),
+                          Text(
+                            '1',
+                            style: GoogleFonts.mulish(color: thirdy),
+                          )
+                        ],
+                      ),
                     ),
-                    child: Row(
+                    Container(
+                      padding: const EdgeInsets.all(7),
+                      width: 30,
+                      height: 30,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(.6),
+                          borderRadius: BorderRadius.circular(50)),
+                      child: Image.asset('assets/icons/bookmark.png'),
+                    )
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('${place.first.name}',
+                        style: GoogleFonts.mulish(fontSize: 18, color: thirdy)),
+                    Row(
                       children: [
-                        Image.asset('assets/icons/award.png', width: 21, height: 21,),
-                        Text('1', style: GoogleFonts.mulish(
-                          color: thirdy
-                        ),)
+                        Image.asset(
+                          'assets/icons/location.png',
+                          width: 20,
+                          height: 20,
+                        ),
+                        Text('${place.first.city}, ${place.first.country}',
+                            style: GoogleFonts.mulish(
+                                color: thirdy.withOpacity(.4))),
                       ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(7),
-                    width: 30,
-                    height: 30,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.withOpacity(.6),
-                      borderRadius: BorderRadius.circular(50)
-                    ),
-                    child: Image.asset('assets/icons/bookmark.png'),
-                  )
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('${place.first.name}', style: GoogleFonts.mulish(
-                      fontSize: 18,
-                      color: thirdy
-                    )),
-                  Row(
-                    children: [
-                      Image.asset('assets/icons/location.png', width: 20, height: 20,),
-                      Text('${place.first.city}, ${place.first.country}', style: GoogleFonts.mulish(
-                      color: thirdy.withOpacity(.4)
-                    )),
-                    ],
-                  )
-                ],
-              )
-            ],
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
-        ),
         ],
       ),
     );
